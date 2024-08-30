962c808151d2e46168407d9d0c93db4bd528ae4c
Konstantin Göksu <konstantin.goksu@elev.ga.ntig.se>
2024-08-30 10:19:07 +0200

    andra

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,52 @@
+﻿string namn;
+bool food1;
+string food = "ee";    //fungerade inte utan ett startvärde
+
+Console.WriteLine("Hej, vad heter du? Karl eller Kalle.");
+
+if (Console.ReadLine() == "Karl")
+{
+    namn = "Karl";
+    food1 = true;
+}
+else
+ {
+namn = "Kalle";
+food1 = false;
+}       
+
+if (food1 == true)
+{
+    Console.WriteLine("Gillar Karl senap eller hundkött");
+    if (Console.ReadLine() == "senap")
+    {
+        food = "senap";
+    }
+    else
+    {
+        food = "hundkött";
+    }
+}
+
+
+if (food1 == false)
+{
+    Console.WriteLine("Gillar Kalle ketchup eller kattkött");   
+    if (Console.ReadLine() == "ketchup")  //kollar vad man skriver, "ketchup" = ketchup och allt annat blir kattkött
+    {
+        food = "ketchup";
+    }
+    else
+    {
+        food = "kattkött";
+    }
+}
+
+
+
+
+Console.WriteLine($"{namn} gillar att äta {food}"); //skriver ut resultatet
+Console.ReadLine();
+
+
+//ctrl + * för snabb kommentar, shift + alt + upp och ner pil för kopiering, alt gr + 7 = {},
