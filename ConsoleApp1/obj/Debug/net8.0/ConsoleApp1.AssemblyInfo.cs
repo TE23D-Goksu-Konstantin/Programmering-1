3d8b5c9fb8f5c08fd16f50d12c90cf1868cc183e
Konstantin Göksu <konstantin.goksu@elev.ga.ntig.se>
2024-09-09 08:50:34 +0200

    Test

--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90884cd0e8eea2c522ad2632b537dd71f1cf76aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4f696994d0ba863a80082267745fc5e93cfbefb")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
